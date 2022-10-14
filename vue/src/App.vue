7e08094b55218a57b2c351522a778b391a25882c
CasperDondersPXL <casper.donders@student.pxl.be>
2022-10-14 10:16:36 +0200

    Added vue example

---
+++ vue/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import { defineCustomElement } from 'vue'
+import HalloWoldComponent from './components/HalloWoldComponent.ce.vue'
+
+// convert into custom element constructor
+const HalloWold = defineCustomElement(HalloWoldComponent)
+
+// register
+customElements.define('hallo-world', HalloWold)
+</script>
+
+<template>
+  <main>
+    <hallo-world name="Kobe" />
+    <hallo-world name="Casper" />
+    <hallo-world name="Ben" />
+    <hallo-world name="Dukan" />
+    <hallo-world name="Wout" />
+    <hallo-world name="Roel" />
+  </main>
+</template>
+
+<style scoped>
+  body {
+    font-family: sans-serif;
+  }
+</style>
---
+++ vue/src/components/HalloWoldComponent.ce.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Hello {{props.name}}</h1>
+    <p>Welcome to our Component</p>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps(["name"]);
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+  font-weight: normal;
+  padding: 1em;
+  margin: 0 0 2em 0;
+  background-color: #eee;
+  border: 1px solid #666;
+}
+
+h1 {
+  font-weight: bold;
+  color: red; 
+}
+</style>
